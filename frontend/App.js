d1c96c4cf613ddedfab6aa1955ae9dad8012e268
Pratapareddi <pratap.reddi99@gmail.com>
2026-03-31 17:30:08 +0530

    add

---
+++ frontend/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>DevOps CI/CD Project</h1>
+      <p>Frontend Running Successfully 🚀</p>
+    </div>
+  );
+}
+
+export default App;
