fb37fd60a41f40efe81fa32787410366f1771f3a
Pratapareddi <pratap.reddi99@gmail.com>
2026-03-31 13:39:04 +0530

    add all files to github

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://mongo:27017/tasks");
+
+const Task = mongoose.model("Task", { name: String });
+
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find();
+  res.json(tasks);
+});
+
+app.post("/tasks", async (req, res) => {
+  const task = new Task({ name: req.body.name });
+  await task.save();
+  res.json(task);
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  await Task.findByIdAndDelete(req.params.id);
+  res.send("Deleted");
+});
+
+app.listen(5000, () => console.log("Backend running"));
